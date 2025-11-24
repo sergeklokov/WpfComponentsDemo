ec4158d9eae10ec13d3268329a864e0f2da4caf4
sam-klok <sklok11@gmail.com>
2025-11-24 09:01:34 -0700

    + Simple Button user control

---
+++ WpfComponentsLibrary/SimpleButton.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfComponentsDemo
+{
+    public partial class SimpleButton : UserControl
+    {
+        public SimpleButton()
+        {
+            InitializeComponent();
+        }
+
+        // Dependency Properties (this is the proper WPF way)
+
+        public static readonly DependencyProperty ButtonTextProperty =
+            DependencyProperty.Register("ButtonText", typeof(string), typeof(SimpleButton), new PropertyMetadata("Click Me"));
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(object), typeof(SimpleButton), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(SimpleButton), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(object), typeof(SimpleButton), new PropertyMetadata(null));
+
+        public string ButtonText
+        {
+            get => (string)GetValue(ButtonTextProperty);
+            set => SetValue(ButtonTextProperty, value);
+        }
+
+        public object Icon
+        {
+            get => GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public object CommandParameter
+        {
+            get => GetValue(CommandParameterProperty);
+            set => SetValue(CommandParameterProperty, value);
+        }
+    }
+}
